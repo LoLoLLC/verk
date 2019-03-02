c6ee3584b999e117ca90561f8058b2bfb4723006
Josh Crews <josh@joshcrews.com>
2019-03-02 09:12:55 -0600

    allow startup env_vars to be used as integers and not strings

--- lib/verk/manager_supervisor.ex
+++ lib/verk/manager_supervisor.ex
@@ -5,9 +5,20 @@ defmodule Verk.Manager.Supervisor do
   @doc false
   def start_link, do: Supervisor.start_link(__MODULE__, [], name: __MODULE__)
 
+  # allows commands like `VERK_DEFAULT_QUEUE=2 iex -S mix` to work because othewise the arg would come in as "2" and be ignored
+  def to_integer(val) when is_bitstring(val) do
+    String.to_integer(val)
+  end
+
+  def to_integer(val) do
+    val
+  end
+
   @doc false
   def init(_) do
     queues = Confex.get_env(:verk, :queues, [])
+             |> Enum.map(fn({k,v}) -> {k, to_integer(v)} end)
+
     children = for {queue, size} <- queues, do: Verk.Queue.Supervisor.child_spec(queue, size)
 
     children = [worker(Verk.Manager, [queues], id: Verk.Manager) | children]
